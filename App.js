58e65c9b52f9db8299c935b2a11cf8a5bcf3382a
giprim <gi4real.gi@gmail.com>
2021-04-30 21:19:46 +0100

    feat: implement AppText

--- App.js
+++ App.js
@@ -1,28 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+import WelcomeScreen from './app/screens/welcome-screen';
 
 export default function App() {
-  const handlePress = () => console.log('Text has been press');
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text} onPress={handlePress}>
-        {' '}
-        Let's get started!!!
-      </Text>
-      <StatusBar style='auto' />
-    </SafeAreaView>
-  );
+  return <WelcomeScreen></WelcomeScreen>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#222',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#f00',
-  },
-});
